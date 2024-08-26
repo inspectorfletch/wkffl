320b6ee5b5e44231c162e08ca2a9b3c17f5e7380
Josh Wise <josh.wise@outlook.com>
2024-08-25 18:20:07 -0700

    Add C# Salary Gen Script using Rolling Averages

---
+++ Salaries_v2/SalaryGen/Players/RankingPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Wkffl.SalaryGen.Players
+{
+    internal enum RankingsType
+    {
+        Ppr,
+        Hppr,
+        Std,
+    }
+
+    internal class SalaryPlayer
+    {
+        public readonly RankingPlayer Player;
+        public readonly double Salary;
+
+        public SalaryPlayer(RankingPlayer player, double salary)
+        {
+            this.Player = player;
+            this.Salary = salary;
+        }
+    }
+
+    internal class RankingPlayer
+    {
+        private readonly Dictionary<RankingsType, int> _overallRankings = new Dictionary<RankingsType, int>();
+        private readonly Dictionary<RankingsType, int> _positionRankings = new Dictionary<RankingsType, int>();
+        private readonly Dictionary<RankingsType, double> _avgRankings = new Dictionary<RankingsType, double>();
+
+        public readonly string Name;
+        public readonly string Position;
+        public readonly string Team;
+
+        public string Identifier { get { return $"{this.Position} {this.Name.ToUpper()} ({this.Team})"; } }
+
+        public double OverallRanking { get { return this._overallRankings.Values.Average(); } }
+        public double PositionRanking { get { return this._positionRankings.Values.Average(); } }
+        public double AvgRanking { get { return this._avgRankings.Values.Average(); } }
+
+        public RankingPlayer(string name, string position, string team)
+        {
+            this.Name = name;
+            this.Position = position;
+            this.Team = team;
+        }
+
+        public void AddRanking(RankingsType type, int overallRanking, int positionRanking, double avgRanking)
+        {
+            this._overallRankings.Add(type, overallRanking);
+            this._positionRankings.Add(type, positionRanking);
+            this._avgRankings.Add(type, avgRanking);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Position} {this.Name} ({this.Team}): {this.OverallRanking} overall, {this.Position}{this.PositionRanking} ({this.AvgRanking})";
+        }
+    }
+}
